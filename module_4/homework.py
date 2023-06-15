0b12afca2e1899262e47e43f1aba251140d13089
Yevhenii3145 <100586000+Yevhenii3145@users.noreply.github.com>
2023-06-15 17:29:26 +0300

    1

---
+++ module_4/homework.py
@@ -0,0 +1,7 @@
+zoo = ('python', 'elephant', 'penguin')
+new_zoo = ('monkey', 'camel', zoo)
+print(new_zoo)
+shoplist = ['apple', 'mango', 'carrot', 'banana']
+print('Еелементи з 1 по 3 це', shoplist[1:3])
+print('Еелементи з 1 по -1 це', shoplist[1:-1])
+print([x for x in range(10) if x % 2 == 0])
